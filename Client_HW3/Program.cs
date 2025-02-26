02864b1d477d54afa77a1a53067a523e0c8d7b65
NazaretsInfinity <157612924+NazaretsInfinity@users.noreply.github.com>
2025-02-26 19:10:15 +0500

    Add project files.

---
+++ Client_HW3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_HW3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string server = "127.0.0.1";
+            int port = 1202;
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse(server), port);
+            Console.WriteLine("Welcome to our badass cookin' center");
+
+            while (true)
+            {
+                Console.WriteLine("Enter ingredients you have, then push ENTER");
+                string message = Console.ReadLine();
+                if (message == "no") break;
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                udpClient.Send(data, data.Length, serverEP);
+
+                string response = Encoding.UTF8.GetString(udpClient.Receive(ref serverEP));
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(response);
+                Console.ForegroundColor= ConsoleColor.Gray;
+            }
+            udpClient.Close();
+
+        }
+    }
+}
---
+++ NetworkServerClient2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkServerClient2
+{
+    class Course
+    {
+        public string Instruction { get; set; }
+        public string[] Ingredients {  get; set; }
+        
+        public Course(string instruction, string[] ingredients)
+        {
+            this.Instruction = instruction;
+            this.Ingredients = ingredients;
+        }
+    }
+
+    class Client
+    {
+        public int AvailableRequests {get; set;}
+        public IPEndPoint Address { get; set;}
+
+        public Client(int availableRequests, IPEndPoint address)
+        {
+            AvailableRequests = availableRequests;
+            Address = address;
+        }
+    }
+    internal class Program
+    {
+
+        private static Dictionary<string, Course> recipes = new Dictionary<string, Course>()
+        {
+            {"fuflo" , new Course("Mix all ingredients end pour down on your head", new string[]{"dirt", "pepper" }) },
+            {"Fried egg", new Course("Crack the egg and pour into a preheated frying pan.\nAdd salt and cook for 5 minutes.",  new string[]{"egg","eggs","salt"}) }
+        };
+
+        
+         private static List<IPEndPoint> logs = new List<IPEndPoint>();
+         private static int PlaceAvailable = 5;
+        //=============== EXTRA MEMBERS END ============
+
+        static void Main(string[] args)
+        {
+            int port = 1202;
+            UdpClient udpServer = new UdpClient(port);
+            Console.WriteLine("Sever active");
+
+
+            while(true)
+            {
+                IPEndPoint clientPoint = new IPEndPoint(IPAddress.Any, port);
+                if (!logs.Contains(clientPoint))
+                    if (PlaceAvailable != 0)
+                    {
+                        logs.Add(clientPoint);
+                        PlaceAvailable--;
+                    }
+                    else
+                    {
+                        byte[] deny = Encoding.UTF8.GetBytes("Server is full rn");
+                        udpServer.Send(deny, deny.Length, clientPoint);
+                        continue;
+                    }
+                
+                
+                    byte[] receivedBytes = udpServer.Receive(ref clientPoint);
+                    string received = Encoding.UTF8.GetString(receivedBytes);
+
+                    string RecipesToSend = GetRecipes(received);
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(RecipesToSend);
+                    udpServer.Send(responseBytes, responseBytes.Length, clientPoint);
+                
+            }
+
+            
+        }
+        private static string GetRecipes(string forrecipe)
+        {
+            List<string> result = new List<string>();
+            string[] ingredients = forrecipe.Split(',');
+            
+            foreach(var recipe in recipes)
+                if(ingredients.All(ing => recipe.Value.Ingredients.Contains(ing.Trim())))
+                    result.Add(recipe.Key);
+
+            // thought to convert the list in a string for console output right here
+            if (result.Count == 0) return "Srs? what you thought you could do with this?\n";
+
+            string response = "";
+            foreach(string recipe in result)
+                response += recipe + "\n" + recipes[recipe].Instruction + "\n";
+            
+            return response;
+        }
+    }
+}
